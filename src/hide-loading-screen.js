4196ae39fbaac8b5152cb7eb5db9407539d90bbb
gweil <60149313+theloniouss@users.noreply.github.com>
2023-05-04 20:41:53 +0200

    Remplacement projet

--- src/hide-loading-screen.js
+++
@@ -1,8 +0,0 @@
-let showEnterButton = () => {
-    document.querySelector('a-scene').style.display = 'none';
-    let button = document.getElementById('enter-ar-button');
-    button.style.display = 'inline-block';
-    button.onclick = switchARMode;
-}
-
-document.addEventListener('renderstart', showEnterButton);
